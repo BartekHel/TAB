7ba4133de168e65d7241811167fa0178c9476146
michkup08 <michal1309k@gmail.com>
2024-04-24 18:35:47 +0200

    auth

--- komis-frontend/src/main.tsx
+++ komis-frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/MainPage.tsx'
 import './index.css'
+import Login from './pages/Login.tsx'
+import Register from './pages/Register.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
