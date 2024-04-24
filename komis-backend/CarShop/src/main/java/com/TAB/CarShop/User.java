62977642005a67bc6dc3acf2a0cc510cf2abb114
wojcikwojtek <wojtekwojcik2310@gmail.com>
2024-04-24 12:45:47 +0200

    Added user class and mysql connection

---
+++ komis-backend/CarShop/src/main/java/com/TAB/CarShop/User.java
@@ -0,0 +1,35 @@
+package com.TAB.CarShop;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id; //Or Integer instead of int
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+}
