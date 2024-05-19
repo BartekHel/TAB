56ba9e96ea78e4ea1515935b8996ea6e6903a726
wojcikwojtek <wojtekwojcik2310@gmail.com>
2024-05-19 22:58:11 +0200

    Poprawiono logowanie i rejestracje i change role

--- komis-backend/CarShop/src/main/java/com/TAB/CarShop/Controllers/UserController.java
+++ komis-backend/CarShop/src/main/java/com/TAB/CarShop/Controllers/UserController.java
@@ -49,34 +49,38 @@ public class UserController {
             Role role = Role.valueOf(regRequest.getRole().toUpperCase());
             User newUser = new User(regRequest.getLogin(), regRequest.getPassword(), regRequest.getEmail(), role, regRequest.getName(), regRequest.getSurname());
             newUser = userRepository.saveAndFlush(newUser);
-
+            long id = 0;
             switch (role) {
                 case CLIENT:
                     Client newClient = new Client();
                     newClient.setUser(newUser);
                     clientRepository.saveAndFlush(newClient);
+                    id = newClient.getClient_id();
                     break;
                 case MANAGER:
                     Manager newManager = new Manager();
                     newManager.setUser(newUser);
                     managerRepository.saveAndFlush(newManager);
+                    id = newManager.getManager_id();
                     break;
                 case REPAIRER:
                     Repairer newRepairer = new Repairer();
                     newRepairer.setUser(newUser);
                     repairerRepository.saveAndFlush(newRepairer);
+                    id = newRepairer.getRepairer_id();
                     break;
                 case DEALER:
                     Dealer newDealer = new Dealer();
                     newDealer.setUser(newUser);
                     dealerRepository.saveAndFlush(newDealer);
+                    id = newDealer.getDealer_id();
                     break;
                 default:
                     return new RegResponse(false, 0, null, null, "Invalid role");
             }
 
 
-            return new RegResponse(true, newUser.getUser_id(), newUser.getLogin(), newUser.getRole(), "Registration successful");
+            return new RegResponse(true, id, newUser.getLogin(), newUser.getRole(), "Registration successful");
         } catch (Exception e) {
             return new RegResponse(false, 0, null, null, e.getMessage());
         }
@@ -89,45 +93,65 @@ public class UserController {
             return new AuthResponse(false, 0, null, null);
         }
         if(user.get(0).getPassword().equals(authRequest.getPassword())) {
-            return new AuthResponse(true, user.get(0).getUser_id(), user.get(0).getLogin() ,user.get(0).getRole());
+            long id = 0;
+            switch (user.get(0).getRole()) {
+                case CLIENT:
+                    id = user.get(0).getClient().getClient_id();
+                    break;
+                case MANAGER:
+                    id = user.get(0).getManager().getManager_id();
+                    break;
+                case REPAIRER:
+                    id = user.get(0).getRepairer().getRepairer_id();
+                    break;
+                case DEALER:
+                    id = user.get(0).getDealer().getDealer_id();
+                    break;
+            }
+            return new AuthResponse(true, id, user.get(0).getLogin() ,user.get(0).getRole());
         } else {
             return new AuthResponse(false, 0, null, null);
         }
     }
 
-    @PutMapping("/chngrole/{id}")
-    User changeRole(@PathVariable Long id, @RequestBody Role role) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            return null;
+    @PutMapping("/chngrole/{login}")
+    long changeRole(@PathVariable String login, @RequestBody Role role) {
+        List<User> user = userRepository.findByLogin(login);
+        if (user.isEmpty()) {
+            return 0;
         }
-        user.setRole(role);
+        user.get(0).setRole(role);
+        long id = 0;
         switch (role) {
             case CLIENT:
                 Client newClient = new Client();
-                newClient.setUser(user);
+                newClient.setUser(user.get(0));
                 clientRepository.saveAndFlush(newClient);
+                id = newClient.getClient_id();
                 break;
             case MANAGER:
                 Manager newManager = new Manager();
-                newManager.setUser(user);
+                newManager.setUser(user.get(0));
                 managerRepository.saveAndFlush(newManager);
+                id = newManager.getManager_id();
                 break;
             case REPAIRER:
                 Repairer newRepairer = new Repairer();
-                newRepairer.setUser(user);
+                newRepairer.setUser(user.get(0));
                 repairerRepository.saveAndFlush(newRepairer);
+                id = newRepairer.getRepairer_id();
                 break;
             case DEALER:
                 Dealer newDealer = new Dealer();
-                newDealer.setUser(user);
+                newDealer.setUser(user.get(0));
                 dealerRepository.saveAndFlush(newDealer);
+                id = newDealer.getDealer_id();
                 break;
             default:
                 System.out.println("Invalid role");
-                return null;
+                return 0;
         }
-        return user;
+        return id;
     }
 
     @PutMapping("/{id}")
