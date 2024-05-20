f7a0acb9b7d33210066b547b6ddec7ee0f0c2214
michkup08 <michal1309k@gmail.com>
2024-05-20 22:30:19 +0200

    poprawa bledu przy pierwszym uruchomieniu + dopasowanie do zmian w bazie danych

--- komis-frontend/src/PageInfo.tsx
+++ komis-frontend/src/PageInfo.tsx
@@ -6,9 +6,10 @@ export const UserProvider = ({ children }) => {
     const [logged, setLogged] = useState(false);
     const [userId, setUserId] = useState(0);
     const [role, setRole] = useState('');
+    const [userLogin, setUserLogin] = useState('');
 
     return(
-        <userContext.Provider value={{logged,setLogged,userId, setUserId, role, setRole}}>
+        <userContext.Provider value={{logged,setLogged,userId, setUserId, role, setRole, userLogin, setUserLogin}}>
             {children}
         </userContext.Provider>
     )
