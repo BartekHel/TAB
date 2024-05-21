ab77d42ad5a57319376d3ff82f7ba115d1cdb1ea
DominikDD30 <91913816+DominikDD30@users.noreply.github.com>
2024-05-21 20:18:07 +0200

    temp

--- komis-frontend/src/pages/RoutesManager.tsx
+++ komis-frontend/src/pages/RoutesManager.tsx
@@ -7,6 +7,7 @@ import Register from './Register'
 import CarDetailsComponent from './CarDetailsComponent'
 import { UserProvider } from '../PageInfo'
 import ServiceOrder from './ServiceOrder'
+import ManagerPage from './ManagerPage'
 const RoutesManager = () => {
   return (
     <div>
@@ -16,6 +17,7 @@ const RoutesManager = () => {
         <Route path="/" element={<App/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/managerPanel" element={<ManagerPage/>}/>
         <Route path="/carDetails/:carId" element={<CarDetailsComponent/>}/>
         <Route path="/orderService" element={<ServiceOrder/>}/>
       </Routes>
